f95ce1ff76dbfb38b7d8bece998f820dd95a1d02
willian emboava <w.emboava.o@gmail.com>
2019-11-26 22:55:34 -0300

    bug fix :: path

--- src/route/index.js
+++ src/route/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Landing from '../pages/Landing';
+import Landing from '../pages/landing';
 import SearchSchool from '../pages/SearchSchool';
 
 export default function IndexRouter() {
